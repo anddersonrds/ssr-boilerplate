cb78406df32011d5fdb6d5f50f1bab00407cfeaf
Anderson Rodrigues <anddersonrds@gmail.com>
2020-09-16 00:58:09 -0300

    add Boilerplate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
-import Main from 'components/Main';
+import * as S from './styles';
 
 export default function Home() {
-  return <Main />;
+  return (
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="Imagem de um átomo e React Avançado escrito ao lado"
+      />
+      <S.Title>Won - Games!</S.Title>
+      <S.Description>Typescript, NextJS e Styled Components</S.Description>
+      <S.Illustration
+        src="/img/hero-illustration.svg"
+        alt="Um desenvolvedor de frente para uma tela de código"
+      />
+    </S.Wrapper>
+  );
 }

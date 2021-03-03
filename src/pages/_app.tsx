390df1f152aeb472a493a13d6ec8d875cd0f461b
Anderson Rodrigues <anddersonrds@gmail.com>
2021-03-03 00:57:46 -0300

    Next-Boilerplate v2.0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,26 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
+import { AppProps } from 'next/app'
+import Head from 'next/head'
 
-import GlobalStyles from 'styles/global';
+import GlobalStyles from 'styles/global'
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Won Games - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <title>AnddersonRds - Boilerplate v2.0</title>
+        <link rel="shortcut icon" href="/img/favicon.ico" />
+        <link rel="apple-touch-icon" href="/img/favicon.ico" />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
-          content="A simple project starter to work with Typescript, React, NextJS and Styled Components"
+          content="A boilerplate configured for all your nexts projects"
         />
       </Head>
       <GlobalStyles />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,3 @@
-import * as S from './styles';
-
 export default function Home() {
-  return (
-    <S.Wrapper>
-      <S.Logo
-        src="/img/logo.svg"
-        alt="Imagem de um átomo e React Avançado escrito ao lado"
-      />
-      <S.Title>Won - Games!</S.Title>
-      <S.Description>Typescript, NextJS e Styled Components</S.Description>
-      <S.Illustration
-        src="/img/hero-illustration.svg"
-        alt="Um desenvolvedor de frente para uma tela de código"
-      />
-    </S.Wrapper>
-  );
+  return <h2>Espero que goste de usar esse boilerplate :)</h2>
 }
